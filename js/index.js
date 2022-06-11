0193d1ae003606d707a814c8fc7f6c41007879ce
Pavel <pavel.chernenko97@gmail.com>
2022-06-11 21:38:33 +0300

    rework all defect and add js

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Tabs 
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__box-wrap");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if (!currentBtn.classList.contains('tabs__nav-btn--active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('tabs__nav-btn--active');
+            })
+            tabsItems.forEach(function(item) {
+                item.classList.remove('tabs__box-wrap--active');
+            })
+            currentBtn.classList.add('tabs__nav-btn--active');
+            currentTab.classList.add('tabs__box-wrap--active')
+        }
+        
+        
+    })
+}
+document.querySelector(".tabs__nav-btn").click();
+
+// price-list
+
+const priceList = document.querySelectorAll(".price__list");
+
+priceList.forEach(onPriceHover);
+
+function onPriceHover(item) {
+    item.addEventListener("mouseover", function() {
+        let currentItem = item;
+        if (!currentItem.classList.contains('price__list--active')) {
+            priceList.forEach(function(item) {
+                item.classList.remove('price__list--active');
+            })
+            currentItem.classList.add('price__list--active');
+        }
+    })
+}
+
+document.querySelector(".price__list:nth-child(2)").click();
